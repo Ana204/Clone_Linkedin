dc73fc0fe48f2bb7c1c029b72bc3c048ea817aa6
ana204 <anagomes.lucia@hotmail.com>
2021-01-08 01:32:04 -0300

    finished head

--- src/components/DesktopHeader/styles.ts
+++ src/components/DesktopHeader/styles.ts
@@ -66,7 +66,20 @@ export const LinkedInIcon = styled(GrLinkedin)`
     flex-shrink: 0;
 `;
 
-export const SearchInput = styled.div ``;  
+export const SearchInput = styled.input `
+    margin-left: 12px;
+    background: var(--color-input);
+    color: var(--color-black);
+    font-size: 14px;
+    padding: 7.5px 8px;
+    border: none;
+    outline: none;
+    border-radius: 2px;
+
+    &:focus{
+        background: var(--color-white);
+    }
+`;  
 
 const generalIconCss = css `
     width: 24px;
