20b4ec845683bef00ff4553ecdeddd3fc03e61f1
ana204 <anagomes.lucia@hotmail.com>
2021-01-11 23:01:22 -0300

    finish clone linkedin

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -23,7 +23,7 @@ const Layout: React.FC = () => {
           <DesktopHeader />
         
           <span>
-            <AdBanner/>
+            {!isLoading &&  <AdBanner/>}
           </span>
 
           <main>
