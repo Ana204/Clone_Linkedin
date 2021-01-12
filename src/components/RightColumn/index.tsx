3bc83e1207f23470c0d8f0784da2bf0e3ff40818
ana204 <anagomes.lucia@hotmail.com>
2021-01-11 22:54:09 -0300

    staged Changes

--- src/components/RightColumn/index.tsx
+++ src/components/RightColumn/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import LoadingTrendingPanel from '../Shimmer/LoadingTrendingPanel';
 import TrendingPanel from './TrendingPanel';
 
 import { Container } from './styles';
+import LoadingFeedPost from '../Shimmer/LoadingFeedPost';
 
-const RightColumn: React.FC<LoadingProps> = ({ isLoading}) => {
+const RightColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="right-column">
-      <TrendingPanel />
-      <TrendingPanel />
+      {isLoading ? (
+        <LoadingTrendingPanel />
+      ) : (
+          <>
+            <TrendingPanel />
+            <TrendingPanel />
+          </>
+        )}
+
     </Container>
   );
 };
